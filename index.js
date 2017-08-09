225c020d6faa34f96a4bde695dcbbf37da554b7c
MAc Ryndziewicz <fuji.two6@gmail.com>
2017-08-09 10:50:00 +0200

    completly changed code

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var fs = require('fs');
 
-fs.readdir('./test', function(err, files){
-    files.forEach(function(file){
-        console.log(file);
-        fs.appendFile('./text.txt', file, function(err){
-            if (err) throw err;
-            console.log('The file has been saved!')
-        });
+fs.readdir('./test', function(err, files) {
+    if (err) throw err
+    console.log('In the folder are following files: ');
+    console.log(files);
+    fs.writeFile('./test.txt', files, function(err, data) {
+        if (err) throw err
+        console.log('Data saved to file.');
     });
 });
